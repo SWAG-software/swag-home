a37a87b6883297582182c146dbf8699175a9a0a9
JuancaJcA <avilafloresjc@gmail.com>
2023-05-11 12:21:44 -0400

    Project created and configurated

---
+++ src/test/kotlin/com/example/swaghome/SwagHomeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.swaghome
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwagHomeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
